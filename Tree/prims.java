ace957ae4bd81656827d74caa14d56354068018c
Spencer Hutchinson <Spencer Hutchinson>
2017-03-14 11:19:24 -0700

    Worked on Tree

--- Tree/prims.java
+++ Tree/prims.java
@@ -6,10 +6,7 @@ public class prims{
 	public static void main(String args[]){
 		Graph graph = new Graph();
 		graph.createGraph("bingraph800-");
-		for(int i = 0; i < graph.vertices.size();i++)	System.out.println(graph.vertices.get(i).index);
-		graph.vertices.add(new Vertex(5));
-		graph.vertices.add(new Vertex(2));
-		graph.vertices.add(new Vertex(3));
+		for(int i = 0; i < graph.vertices.size();i++) for(int j = 0; j < graph.vertices.get(i).connections.size(); j++)	System.out.println("vertex: "+graph.vertices.get(i).index+"\tconnection: "+graph.vertices.get(i).connections.get(j).connection+"\t\tweight: "+graph.vertices.get(i).connections.get(j).weight);
 		//System.out.println(graph.checkUsed(new Vertex(4)));
 	}
 }
@@ -17,6 +14,7 @@ public class prims{
 class Graph{
 
 	ArrayList<Vertex> vertices = new ArrayList();
+	Graph tree = new Graph();
 	
 	void createGraph(String filename){
 		int buff[] = new int[5500];
@@ -28,7 +26,8 @@ class Graph{
 				int conn1 = din.readInt();
 				int conn2 = din.readInt();
 				int weight = din.readInt();
-				din.readInt();
+//				din.readInt();
+	//			System.out.println("vertex: "+conn1+"\tconnection: "+conn2+"\t\tweight: "+weight);
 				Vertex vert1 = new Vertex(conn1);
 				Vertex vert2 = new Vertex(conn2);
 				addVertex(vert1,weight,conn2);
@@ -54,9 +53,45 @@ class Graph{
 			conn.addConnection(weight, conn2);
 			vertices.add(conn);
 		} 
-		else vertices.get(used).addConnection(weight,conn2);
-		
+		else vertices.get(used).addConnection(weight,conn2);	
 	}
+	
+	Vertex find(int index){
+		for(int i = 0; i < vertices.size(); i++){
+			Vertex temp = vertices.get(i);
+			if(index == temp.index) return temp; 
+		}		
+	}
+	
+	int primSolve(int startI){
+		int nextIndex = startI;
+		Vertex temp;
+		for(int i = 0; i < vertices.size(); i++){
+			
+			Vertex add = new Vertex; //the vertex added to the sub tree
+			temp = vertices.get(nextIndex); //the working vertex
+			tree.vertices.add(vertices.get(nextIndex)); //adds vertex to graph
+			Edge least = temp.connections.get(0); //the working edge of the vertex
+			
+			for(int j = 0; j < temp.connections.size(); j++){
+				Edge eTemp = temp.connections.get(j); //the working edge
+				if(eTemp.weight < least.weight && tree.checkUsed(temp) == -1) least = eTemp; //the least edge
+			}
+			
+			nextIndex = least.connection;
+		}
+	}
+/* 	
+	boolean checkCycle(Graph G, int Vertex){
+		if(int i = 0; i < vertices.size(); i++){
+			
+		}
+	}
+	
+	boolean recurse(Vertex current, Graph tree){
+		
+		for(int i = 0; i < current.connections.size())
+	} */
 }
 
 class Vertex{
@@ -70,7 +105,6 @@ class Vertex{
 	void addConnection(int weight, int connection){
 		connections.add(new Edge(weight, connection));
 	}
-	
 }
 
 class Edge{
